2683dff3dd706964f607c677f2be4be3b35248a4
Andreas Karlsson <andreas.karlsson@indiebytes.se>
2020-09-04 12:23:34 +0200

    Update Storyblok logic

--- builtin/forStoryblok.ts
+++ builtin/forStoryblok.ts
@@ -1,15 +1,37 @@
+import config from 'config'
+import { removeStoreCodeFromRoute } from '@vue-storefront/core/lib/multistore'
+import storeCodeFromRoute from '@vue-storefront/core/lib/storeCodeFromRoute'
+import get from 'lodash-es/get'
 import { Payload } from '../types/Payload'
 
-export const forStoryblok = async ({ dispatch }, { url: fullSlug }: Payload) => {
-  fullSlug = fullSlug || 'home'
-  const story = await dispatch(`storyblok/loadStory`, { fullSlug }, { root: true })
-  if (story && story.story !== false) {
-    return {
-      name: 'storyblok',
-      params: {
-        // TODO: Why does this need to be here?
-        slug: 'storyblok'
-      }
+const route = {
+  name: 'storyblok',
+  params: {
+    slug: 'storyblok'
+  }
+}
+
+export const forStoryblok = async ({ dispatch, rootState }, { url, params }: Payload) => {
+  if (params && params._storyblok_c && params._storyblok_c === 'page') {
+    route.params = {
+      ...route.params,
+      ...params
     }
+
+    return route
+  }
+  url = url.replace(/\/?(\?.*)?$/, '') // remove trailing slash and/or qs variables if present
+  const storeCode = storeCodeFromRoute(url)
+  const addStoreCode = get(config, 'storyblok.settings.appendStoreCodeFromHeader')
+  const storeCodeToAdd = rootState.storyblok.storeCode
+  if (addStoreCode && storeCodeToAdd) {
+    url = `${storeCodeToAdd}/${(url || 'home')}`
+  }
+  if (config.storeViews.multistore && storeCode && url.replace(/\/$/, '') === removeStoreCodeFromRoute(url)) {
+    url = `${url}/home`
+  }
+  const story = await dispatch(`storyblok/loadStory`, { fullSlug: url }, { root: true })
+  if (story) {
+    return route
   }
 }
